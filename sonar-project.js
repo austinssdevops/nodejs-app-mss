d90dd1bd87488624f53ca35312311b25506ef8a2
Sreehari <sreehari.gaddam@gmail.com>
2022-12-30 10:41:08 -0600

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.251.77.202:9000/',
+  serverUrl: 'http://192.168.1.159:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'squ_5f2b52d72f4f2f7054397246b36ac22e7d1a35a2',
+	    'sonar.login': 'squ_2e85f788b63c041ab5264ee8f52ee32cfdf40659',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
